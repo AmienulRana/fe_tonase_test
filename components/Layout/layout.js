f25d98402afec5c72e888ab2c2827287fc50819f
AmienulRana <amienulrana@gmail.com>
2022-07-09 20:35:50 +0700

    membuat dinamic title

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,17 +1,20 @@
 import Navbar from "../element/Navbar";
 import Footer from '../element/Footer';
 import Head from 'next/head';
-export default function Layout({ children }){
+export default function Layout({ children, title = 'Home' }){
     return(
-        <div className="container">
+        <>
             <Head>
+                <title>{title}</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
             </Head>
-            <Navbar />
-            <main>
-                { children }
-            </main>
-            <Footer />
-        </div>
+            <div className="container">
+                <Navbar />
+                <main>
+                    { children }
+                </main>
+                <Footer />
+            </div>
+        </>
     )
 }
