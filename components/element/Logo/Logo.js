cc6bdf5c697e32e2045a4d49efae3b0b4dbeadd3
AmienulRana <amienulrana@gmail.com>
2022-07-09 20:11:00 +0700

    menghapus semua folder yang ada di dalam folder elemen dan mengeluarkan file yang ada di folder ini ke dalam folder elemen

--- components/element/Logo/Logo.js
+++
@@ -1,5 +0,0 @@
-export default function Logo(){
-    return(
-        <h1 className='logo font-bold flex items-center justify-center md:w-14 md:h-14 w-8 h-8 text-white rounded-xl mr-4'>S</h1>
-    )
-}
